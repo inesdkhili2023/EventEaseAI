14acc3a25548d22add69feecdede4bb496ad36b1
inesdkhili2023 <ines.dkhili@esprit.tn>
2025-10-25 16:06:20 +0100

    config de eureka et apigateway

--- EventEase-Ines/src/main/java/com/example/eventeaseines/EventEaseInesApplication.java
+++ EventEase-Ines/src/main/java/com/example/eventeaseines/EventEaseInesApplication.java
@@ -2,8 +2,10 @@ package com.example.eventeaseines;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 //@ComponentScan(basePackages = {"com.example.eventeaseines", "Security"})
 public class EventEaseInesApplication {
